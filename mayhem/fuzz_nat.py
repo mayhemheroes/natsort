f36b929776606614717e36942efb901991941eca
Bailey Capuano <bcapuano@asu.edu>
2023-05-31 21:10:32 -0400

    Defect found

--- mayhem/fuzz_nat.py
+++ mayhem/fuzz_nat.py
@@ -8,17 +8,10 @@ with atheris.instrument_imports():
     from natsort import natsorted, ns
 
 types = [ns.REAL, ns.LOCALE, ns.IGNORECASE]
-ctr = 0
+
 def TestOneInput(data):
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
-    global ctr
-    ctr += 1
-    try:
-        natsorted(fuzz_helpers.build_fuzz_list(fdp, [str]), alg=fdp.PickValueInList(types))
-    except ValueError:
-        if ctr > 100:
-            raise
-        return -1
+    natsorted(fuzz_helpers.build_fuzz_list(fdp, [str]), alg=fdp.PickValueInList(types))
 
 def main():
     atheris.Setup(sys.argv, TestOneInput)
